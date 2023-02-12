6b64eba0f794a36842d2efc906171ef7c167c042
Manas Bajpai <97174936+MacWeTT@users.noreply.github.com>
2023-02-12 13:12:04 +0530

    Initialization

---
+++ BasicTechniques/Introduction/first.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(nullptr);
+}
+int main(int argc, char const *argv[])
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        solve();
+    }
+    return 0;
+}
